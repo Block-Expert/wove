769fa3d99d532276c98e8732e41b9daee9e327fa
Brad Anderson <2575489+banderson12@users.noreply.github.com>
2023-04-10 13:40:12 -0600

    Update Hero.tsx

--- apps/nextjs/src/components/marketing/Hero.tsx
+++ apps/nextjs/src/components/marketing/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
               className="inline-flex space-x-6"
             >
               <span className="rounded-full bg-indigo-600/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-600/10">
-                100% open source
+               Beta Launch
               </span>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
                 <span>Github</span>
